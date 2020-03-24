bf9c1235edd4aed66caa2debf545153c9d797e64
Mathieu Lemieux <mathieu@mathieulemieux.com>
2020-03-24 18:10:23 -0400

    Updates

---
+++ precache-manifest.7951c4d916aae820bf45fc49b3b7a6f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c418c075b804c79fa90b719aeadc2aa9",
+    "url": "/mathieulemieux/snippy/index.html"
+  },
+  {
+    "revision": "5e2ce563b3388a29df5a",
+    "url": "/mathieulemieux/snippy/static/css/main.0d5bde3f.chunk.css"
+  },
+  {
+    "revision": "1540bacbb43ccab0960a",
+    "url": "/mathieulemieux/snippy/static/js/2.cdfdd7c8.chunk.js"
+  },
+  {
+    "revision": "90fb4deb912422b947a6490378160cce",
+    "url": "/mathieulemieux/snippy/static/js/2.cdfdd7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2ce563b3388a29df5a",
+    "url": "/mathieulemieux/snippy/static/js/main.e13e5781.chunk.js"
+  },
+  {
+    "revision": "81be87ee6ccb2f2b77cc",
+    "url": "/mathieulemieux/snippy/static/js/runtime-main.df46020c.js"
+  }
+]);
