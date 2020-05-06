cfe9303829b877cdfac62a88c80fb1ac592cd9f6
Mathieu Lemieux <mathieu@mathieulemieux.com>
2020-05-06 09:27:15 -0400

    Updates

--- precache-manifest.e22b116f2c899f1960cd2dd2a22430b8.js
+++ precache-manifest.e22b116f2c899f1960cd2dd2a22430b8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "88b2dc28e9ed2c6abc501511397ae19f",
+    "revision": "11f64c063316257c7467778e9a18c717",
     "url": "/snippy/index.html"
   },
   {
-    "revision": "d033167ffa092ee92293",
+    "revision": "8263a4772e28d1be0b99",
     "url": "/snippy/static/css/main.0d5bde3f.chunk.css"
   },
   {
-    "revision": "c175da1aa9ab8a18af37",
-    "url": "/snippy/static/js/2.d50a2ad3.chunk.js"
+    "revision": "a450228edac296089fc5",
+    "url": "/snippy/static/js/2.4e3eb8fd.chunk.js"
   },
   {
     "revision": "90fb4deb912422b947a6490378160cce",
-    "url": "/snippy/static/js/2.d50a2ad3.chunk.js.LICENSE.txt"
+    "url": "/snippy/static/js/2.4e3eb8fd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d033167ffa092ee92293",
-    "url": "/snippy/static/js/main.547171ff.chunk.js"
+    "revision": "8263a4772e28d1be0b99",
+    "url": "/snippy/static/js/main.a0c53e5d.chunk.js"
   },
   {
     "revision": "475b8fb4edc50feb3036",
